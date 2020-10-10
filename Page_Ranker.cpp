4a546b7123ed94a0fe853e405ae258ed66541e63
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-10-10 11:20:40 -0700

    refactored functions into Page_Ranker

---
+++ Page_Ranker.cpp
@@ -0,0 +1,74 @@
+/**
+ * 10/2/2020
+ * Created by Karel Chanivecky Garcia.
+ * A01052674
+ *
+ * BCIT CST
+ * Set O Datacomm
+ * COMP - 3522
+ *
+ * Assignment 1
+ */
+
+#include "Page_Ranker.hpp"
+#include <iostream>
+#include <sstream>
+#include <cstring>
+#include <cmath>
+#include <fstream>
+
+
+
+constexpr int INITIAL_PAGE_NAME = 'A';
+constexpr int CONNECTED = 1;
+
+/**
+ * Read connections from file.
+ * @param filename the name of the file to read connections from
+ * @return a vector of int representing connections
+ */
+vector<int> * get_connections(string &filename) {
+    string line;
+    ifstream src{filename};
+    auto connections = new vector<int>;
+    if (!src.is_open()) {
+        cerr << "could not open file:\n" << strerror(errno) << endl;
+        exit(EXIT_FAILURE);
+    }
+    while (!src.eof()) {
+        getline(src, line);
+        istringstream iss{line};
+        int conn;
+        iss >> conn;
+        while (!iss.eof()) {
+            connections->push_back(conn);
+            iss >> conn;
+        }
+    }
+}
+
+/**
+ * Assemble pages.
+ * @param connections the connections to assemble pages from
+ * @return a vector of string
+ */
+vector<string> * assemble_pages(vector<int> &connections) {
+    double number_of_pages = sqrt(connections.size());
+    if ((number_of_pages - (int) number_of_pages) != 0) {
+        throw runtime_error("File is corrupt!");
+    }
+    auto * pages = new vector<string>;
+    for ( int i = 0; i < number_of_pages; ++i ) {
+        string name{to_string(INITIAL_PAGE_NAME + i)};
+        pages->emplace_back(name);
+    }
+}
+
+/**
+ * Get a Page_Matrix where the values are the rank of the respective pages
+ * @param pages the pages to rank
+ * @return a Page_Matrix
+ */
+Page_Matrix * rank_pages(Stochastic_Matrix &sto_matrix) {
+
+}
---
+++ Page_Ranker.hpp
@@ -0,0 +1,44 @@
+/**
+ * 10/2/2020
+ * Created by Karel Chanivecky Garcia.
+ * A01052674
+ *
+ * BCIT CST
+ * Set O Datacomm
+ * COMP - 3522
+ *
+ * Assignment 1
+ */
+
+#ifndef ASSIGNMENT1_V2_PAGE_RANKER_HPP
+#define ASSIGNMENT1_V2_PAGE_RANKER_HPP
+
+
+#include <string>
+#include <vector>
+#include "Page_Matrix.hpp"
+#include "Stochastic_Matrix.hpp"
+
+using namespace std;
+/**
+ * Read connections from file.
+ * @param filename the name of the file to read connections from
+ * @return a vector of int representing connections
+ */
+vector<int> * get_connections(string &filename);
+
+/**
+ * Assemble pages.
+ * @param connections the connections to assemble pages from
+ * @return a vector of string
+ */
+vector<string> * assemble_pages(vector<int> &connections);
+
+/**
+ * Get a Page_Matrix where the values are the rank of the respective pages
+ * @param pages the pages to rank
+ * @return a Page_Matrix
+ */
+Page_Matrix * rank_pages(Stochastic_Matrix &sto_matrix);
+
+#endif //ASSIGNMENT1_V2_PAGE_RANKER_HPP
